7fdcb85a1bcd743f4e2b764adb979e7ad5a7eb3b
Dragos Calin <calinho_gx1992@yahoo.com>
2014-02-04 19:07:45 +0200

    Limit input with get_action

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,5 +1,12 @@
 require 'restaurant'
 class Guide
+  
+  class Config
+    @@actions = ['list', 'find', 'add', 'quit']
+
+    def self.actions; @@actions; end
+  end
+
   def initialize(path=nil)
     # locate the restaurant text file at path
     Restaurant.file_path = path
@@ -20,13 +27,22 @@ class Guide
     # action loop
     result = nil
     until result == :quit
-      #   what do you want to do? (list, find, add, quit)
-      print '> '
-      user_response = gets.chomp
+      action = get_action
       #   do that action
-      result = do_action(user_response)
+      result = do_action(action)
     end
     conclusion
+  end
+
+  def get_action
+    action = nil
+    until Guide::Config.actions.include? action
+      puts "Actions " + Guide::Config.actions.join(', ') if action
+      print '> '
+      user_response = gets.chomp
+      action = user_response.downcase.strip
+    end
+    return action
   end
 
   def do_action(action)
